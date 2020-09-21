c4832447db6be793e787f5554e87e686c1f8a635
Millione <isjieliu@163.com>
2020-09-21 12:07:16 +0800

    set up the enviroment

---
+++ src/test/hashmap/HashMapTest.java
@@ -0,0 +1,13 @@
+package test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 1);
+    }
+
+}
